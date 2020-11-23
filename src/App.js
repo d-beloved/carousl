90b78d83ebb4d3ae58dc3f38ba28f3b9f9f43ea7
d-beloved <morayodeji@gmail.com>
2020-11-23 13:49:11 +0100

    add the buttons and click event

--- src/App.js
+++ src/App.js
@@ -1,53 +1,80 @@
 import React, { useRef } from "react";
 import Card from "./Card";
 import "./App.css";
+import leftCircle from "./asset/leftarrow.svg";
+import rightCircle from "./asset/rightarrow.svg";
 
 const CardArray = [
-  {name: 'first card'},
-  {name: 'second card'},
-  {name: 'third card'},
-  {name: 'fourth card'},
-  {name: 'fifth card'},
-  {name: 'sixth card'},
-  {name: 'seventh card'}
-]
+  { name: "first card" },
+  { name: "second card" },
+  { name: "third card" },
+  { name: "fourth card" },
+  { name: "fifth card" },
+  { name: "sixth card" },
+  { name: "seventh card" },
+];
 
 function App() {
   let carouslRef = useRef(null);
 
   const prev = () => {
-    carouslRef &&  carouslRef.current && carouslRef.current.scrollTo({
-      behavior: 'smooth',
-      top: 0,
-      left: carouslRef.current.scrollLeft - carouslRef.current.clientWidth * 0.5
-    })
-  }
+    carouslRef &&
+      carouslRef.current &&
+      carouslRef.current.scrollTo({
+        behavior: "smooth",
+        top: 0,
+        left:
+          carouslRef.current.scrollLeft - carouslRef.current.clientWidth * 0.5,
+      });
+  };
 
   const next = () => {
-    carouslRef &&  carouslRef.current && carouslRef.current.scrollTo({
-      behavior: 'smooth',
-      top: 0,
-      left: carouslRef.current.scrollLeft + carouslRef.current.clientWidth * 0.5
-    })
-  }
+    carouslRef &&
+      carouslRef.current &&
+      carouslRef.current.scrollTo({
+        behavior: "smooth",
+        top: 0,
+        left:
+          carouslRef.current.scrollLeft + carouslRef.current.clientWidth * 0.5,
+      });
+  };
 
   return (
-    <div className="App">
-      <span>
-        <button onClick={prev}>
-          <img src={leftarrCircle} alt="left button" />
-        </button>
-        <button onClick={next}>
-          <img src={rightarrCircle} alt="right button" />
-        </button>
-      </span>
+    <div
+      style={{
+        display: "flex",
+        margin: "20rem 0",
+      }}
+    >
+      <button
+        style={{
+          background: "none",
+          border: "none",
+          outline: "none",
+          padding: "0",
+        }}
+        onClick={prev}
+      >
+        <img style={{ width: "10%" }} src={leftCircle} alt="left button" />
+      </button>
       <div>
-        <main ref={carouslRef}>
+        <main style={{ display: "flex" }} ref={carouslRef}>
           {CardArray.map((carousel, i) => (
             <Card {...carousel} key={i} />
           ))}
         </main>
       </div>
+      <button
+        style={{
+          background: "none",
+          border: "none",
+          outline: "none",
+          padding: "0",
+        }}
+        onClick={next}
+      >
+        <img style={{ width: "10%" }} src={rightCircle} alt="right button" />
+      </button>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Card = ({ name }: props) => {
-  <div style={{ color: "black" }}>
-    <p>{name}</p>
-  </div>;
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <p style={{ color: 'white' }}>{name}</p>
+    </div>
+  );
 };
 
 export default Card;
